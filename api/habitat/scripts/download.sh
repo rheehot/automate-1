b8c20ac82a146334cd17f30554902c5f8157d99f
kmacgugan <kmacgugan@chef.io>
2019-05-09 10:47:03 -0700

    Add studio helper, protos and generated code for habitat messages

---
+++ api/habitat/scripts/download.sh
@@ -0,0 +1,7 @@
+curl --header "Authorization: token $GITHUB_TOKEN" \
+     --header 'Accept: application/vnd.github.v3.raw' \
+     --remote-name \
+     --location 'https://raw.githubusercontent.com/habitat-sh/habitat/master/components/sup-protocol/protocols/types.proto'
+
+mv types.proto api/habitat/
+mv event.proto api/habitat/
